2df7fc9845409d3c3a9480eac64f4e2760a36aa6
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-03 20:48:52 -0500

    Add input and canvas size feature in css and js
    This commit adds a CSS variable for sizing the pixel class within the
    div container. There was a good bit of JS commited as well for handling
    user input for the grid size, creating a new canvas sized to the user
    input, and passing the user input to the css variable to handle the new
    size for the pixel class.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function createGrid(size = 16) {
+  const divContainer = document.querySelector(".div-container");
+  divContainer.innerHTML = "";
+
   for (let i = 0; i < size * size; i++) {
     let div = document.createElement("div");
     div.classList.add("pixel");
-    document.querySelector(".div-container").appendChild(div);
+    divContainer.appendChild(div);
   }
 }
 
@@ -15,3 +18,21 @@ const container = document.querySelector(".div-container");
 container.addEventListener("mouseover", (e) => {
   e.target.setAttribute("style", "background: #888888");
 });
+
+function createNewCanvas() {
+  let newSize = getUserInput();
+  createGrid(newSize);
+  passUserInput(newSize);
+}
+
+function getUserInput() {
+  return +prompt("Please enter a grid size:", 16);
+}
+
+document.querySelector("#new-canvas").addEventListener("click", () => {
+  createNewCanvas();
+});
+
+function passUserInput(param) {
+  document.documentElement.style.setProperty("--size-input", param);
+}
